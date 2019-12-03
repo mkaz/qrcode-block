01a1fdf98390442083d0a8e0a05edb06c8dac536
Marcus Kazmierczak <marcus@mkaz.com>
2019-12-02 20:57:28 -0800

    Add attributes, TextControl

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
-
 import { registerBlockType } from '@wordpress/blocks';
+import { TextControl } from '@wordpress/components';
 
 registerBlockType( 'mkaz/qrcode-block', {
 	title: 'QRCode Block',
 	icon: 'visibility',
 	category: 'widgets',
-	edit: () => {
+	attributes: {
+		url: {
+			type: 'string',
+			source: 'text',
+			selector: '.theurl',
+		},
+	},
+	edit: ( { attributes, setAttributes } ) => {
 		return (
-			<div> Hello in Editor. </div>
+			<div>
+				<TextControl
+					label="URL"
+					value={ attributes.url }
+					onChange={ ( val ) => setAttributes( { url: val } ) }
+				/>
+			</div>
 		);
 	},
-	save: () => {
+	save: ( { attributes } ) => {
 		return (
-			<div> Hello in Save. </div>
+			<div className="theurl">{ attributes.url }</div>
 		);
 	}
 });
