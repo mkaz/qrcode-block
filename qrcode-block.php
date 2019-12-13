dc102ed79624f278c7a2ec3367033c9289afeb0e
Marcus Kazmierczak <marcus@mkaz.com>
2019-12-12 19:02:20 -0800

    Whitespace

--- qrcode-block.php
+++ qrcode-block.php
@@ -13,29 +13,29 @@
 
  add_action( 'init', function() {
 
-    $asset_file = include( plugin_dir_path( __FILE__ ) . 'build/index.asset.php' );
+	$asset_file = include( plugin_dir_path( __FILE__ ) . 'build/index.asset.php' );
 
-    wp_register_script( 'mkaz-qrcode-block-script',
-        plugins_url( 'build/index.js', __FILE__ ),
-        $asset_file['dependencies'],
-        $asset_file['version']
-    );
+	wp_register_script( 'mkaz-qrcode-block-script',
+		plugins_url( 'build/index.js', __FILE__ ),
+		$asset_file['dependencies'],
+		$asset_file['version']
+	);
 
-    register_block_type( 'mkaz/qrcode-block', array(
-        'editor_script' => 'mkaz-qrcode-block-script',
-    ));
+	register_block_type( 'mkaz/qrcode-block', array(
+		'editor_script' => 'mkaz-qrcode-block-script',
+	));
 
  });
 
  add_action( 'wp_enqueue_scripts', function() {
-    wp_enqueue_script( 'mkaz-qrcode-qrcodejs',
-        plugins_url( 'qrcode.min.js', __FILE__ )
-    );
+	wp_enqueue_script( 'mkaz-qrcode-qrcodejs',
+		plugins_url( 'qrcode.min.js', __FILE__ )
+	);
 
-    wp_enqueue_script( 'mkaz-qrcode-trigger',
-        plugins_url( 'qr-trigger.js', __FILE__ ),
-        array( 'mkaz-qrcode-qrcodejs' ),
-        filemtime( plugin_dir_path( __FILE__ ) . 'qr-trigger.js' ),
-        true // in footer
-    );
+	wp_enqueue_script( 'mkaz-qrcode-trigger',
+		plugins_url( 'qr-trigger.js', __FILE__ ),
+		array( 'mkaz-qrcode-qrcodejs' ),
+		filemtime( plugin_dir_path( __FILE__ ) . 'qr-trigger.js' ),
+		true // in footer
+	);
  });
