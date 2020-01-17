522b89881f2fbc316b41b98ed12a9ed9308a421d
Marcus Kazmierczak <marcus@mkaz.com>
2020-01-16 20:15:54 -0800

    Part 7 - Add placeholder and displaying of qrcode
    Updates edit function to show a placeholder when the URL
    is not set, or if editing, and to show the actual QRCode
    when it is set and the block is not selected.

--- src/edit.js
+++ src/edit.js
@@ -1,16 +1,45 @@
 import {
+	Placeholder,
 	TextControl,
 } from '@wordpress/components';
 
+import { useEffect } from '@wordpress/element';
 
-const edit = ( { attributes, setAttributes } ) => {
+const edit = ( { attributes, isSelected, setAttributes } ) => {
+	useEffect( () => {
+		if ( !isSelected ) {
+			const elem = document.getElementById("qrcode");
+			if ( elem ) {
+				const url = elem.dataset.url; // reads data-url
+				const qrcode = new QRCode(elem, {
+					text: url,
+					width: 256,
+					height: 256,
+					colorDark : "#000000",
+					colorLight : "#ffffff",
+					correctLevel : QRCode.CorrectLevel.H
+				});
+			}
+		}
+	});
+
 	return (
 		<div>
-			<TextControl
-				label="URL"
-				value={ attributes.url }
-				onChange={ ( val ) => setAttributes( { url: val } ) }
-			/>
+			{ attributes.url && ! isSelected
+				? <div
+					id="qrcode"
+					data-url={ attributes.url }
+				></div>
+				: <Placeholder
+					label="QRCode Block"
+					instructions="Add URL for QRCode"
+				>
+					<TextControl
+						value={ attributes.url }
+						onChange={ ( val ) => setAttributes( { url: val } ) }
+					/>
+				</Placeholder>
+			}
 		</div>
 	);
 };
